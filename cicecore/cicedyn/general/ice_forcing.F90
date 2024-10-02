12ca2e5798fbf959653861333c1f2ab9bfd5cd32
Jean-Francois Lemieux <jean-francois.lemieux@canada.ca>
2024-10-02 17:22:18 +0000

    Changed label to SW for the new wind stress

--- cicecore/cicedyn/general/ice_forcing.F90
+++ cicecore/cicedyn/general/ice_forcing.F90
@@ -319,8 +319,8 @@
          call uniform_data_atm('S')
       elseif (trim(atm_data_type) == 'uniform_west') then
          call uniform_data_atm('W')
-      elseif (trim(atm_data_type) == 'uniform_NWW') then
-         call uniform_data_atm('NWW')
+      elseif (trim(atm_data_type) == 'uniform_SW') then
+         call uniform_data_atm('SW')
       elseif (trim(atm_data_type) == 'calm') then
          call uniform_data_atm('N',c0) ! direction does not matter when c0
       elseif (trim(atm_data_type) == 'hycom') then
@@ -657,8 +657,8 @@
          call uniform_data_atm('S')
       elseif (trim(atm_data_type) == 'uniform_west') then
          call uniform_data_atm('W')
-      elseif (trim(atm_data_type) == 'uniform_NWW') then
-         call uniform_data_atm('NWW')
+      elseif (trim(atm_data_type) == 'uniform_SW') then
+         call uniform_data_atm('SW')
       elseif (trim(atm_data_type) == 'calm') then
          call uniform_data_atm('N',c0) ! direction does not matter when c0
       elseif (trim(atm_data_type) == 'hycom') then
@@ -5297,7 +5297,7 @@
       elseif (dir == 'W') then
          uatm = -atm_val
          vatm = c0
-      elseif (dir == 'NWW') then
+      elseif (dir == 'SW') then
          uatm = -atm_val
          vatm = -atm_val
       else
